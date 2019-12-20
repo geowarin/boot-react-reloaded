d95bb4d14ee7676072122415216ba1e025b9fe10
Geoffroy Warin <code@geowarin.com>
2019-12-20 04:53:45 +0100

    Style the form

--- modules/frontend/src/pages/LoginForm.tsx
+++ modules/frontend/src/pages/LoginForm.tsx
@@ -3,6 +3,7 @@ import {useForm} from "react-hook-form";
 import * as yup from "yup";
 import {api} from "../api";
 import {useLocation} from "wouter";
+import classNames from "classnames";
 
 interface Props {
 }
@@ -15,24 +16,35 @@ const validationSchema = yup.object({
 type FormData = yup.InferType<typeof validationSchema>;
 
 const LoginForm: React.FC<Props> = (props) => {
-  const {register, handleSubmit, errors} = useForm<FormData>({
-    validationSchema
-  });
+  const {register, handleSubmit, errors} = useForm<FormData>({validationSchema});
   const [location, setLocation] = useLocation();
+
   const onSubmit = handleSubmit(({userName, password}) => {
     api.url("/auth")
       .post({userName, password}).text()
       .then(() => setLocation("/"))
   });
 
+  const hasFieldErrors = !!Object.keys(errors).length;
+  const urlParams = new URLSearchParams(window.location.search);
+  const hasGlobalError = urlParams.has("error");
+  const globalError = !hasFieldErrors && hasGlobalError && "Invalid credentials";
+
   return (
-    <form onSubmit={onSubmit}>
-      <input name="userName" ref={register}/>
-      {errors.userName?.message}
-      <input name="password" ref={register}/>
-      {errors.password?.message}
-      <input type="submit"/>
-    </form>
+    <div className="form-container">
+      <h2>Please login</h2>
+
+      <form onSubmit={onSubmit} className={classNames({"error": hasGlobalError})}>
+        <input tabIndex={0} type="text" autoComplete="off" name="userName" ref={register}/>
+        <div className="error">{errors.userName?.message}</div>
+
+        <input type="password" autoComplete="off" name="password" ref={register}/>
+        <div className="error">{errors.password?.message}</div>
+
+        <div className="error">{globalError}</div>
+        <input type="submit"/>
+      </form>
+    </div>
   );
 };
 
