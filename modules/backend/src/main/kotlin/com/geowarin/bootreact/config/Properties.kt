37425cf9a08b70004f983d77ba7d40716e0f2222
Geoffroy Warin <code@geowarin.com>
2019-12-21 17:49:47 +0100

    Handle empty properties

--- modules/backend/src/main/kotlin/com/geowarin/bootreact/config/Properties.kt
+++ modules/backend/src/main/kotlin/com/geowarin/bootreact/config/Properties.kt
@@ -6,5 +6,5 @@ import org.springframework.boot.context.properties.ConstructorBinding
 @ConstructorBinding
 @ConfigurationProperties(prefix = "com.geowarin.cors")
 data class CorsProperties(
-  val allowedOrigin: List<String>
+  val allowedOrigin: List<String> = emptyList()
 )
